e6b293be65a3ba3356c89893e5b53e74284ffc0f
Clinton <clintonarmando25@gmail.com>
2023-07-13 03:19:39 -0500

    agregando personajes

--- game/main.cpp
+++ game/main.cpp
@@ -3,11 +3,30 @@
 class Game
 {
 public:
-    Game() : window(sf::VideoMode(800, 600), "Mover Cuadrado"), square(sf::Vector2f(100.0f, 100.0f))
+    Game() : window(sf::VideoMode(1200, 900), "Mover Cuadrado"), square(sf::Vector2f(100.0f, 100.0f))
     {
         square.setFillColor(sf::Color::Red);
-        square.setPosition(0.0f, 250.0f);
+        square.setPosition(0.0f, 400.0f);
         squareSpeed = 200.0f;
+        sf::RectangleShape stud1(sf::Vector2f(100.0f, 100.0f));
+    	stud1.setFillColor(sf::Color::Green);
+    	stud1.setPosition(200.0f, 600.0f);
+        sf::RectangleShape stud2(sf::Vector2f(100.0f, 100.0f));
+    	stud2.setFillColor(sf::Color::Green);
+    	stud2.setPosition(200.0f, 200.0f);
+        sf::RectangleShape stud3(sf::Vector2f(100.0f, 100.0f));
+    	stud3.setFillColor(sf::Color::Green);
+    	stud3.setPosition(600.0f, 600.0f);
+        sf::RectangleShape stud4(sf::Vector2f(100.0f, 100.0f));
+    	stud4.setFillColor(sf::Color::Green);
+    	stud4.setPosition(600.0f, 200.0f);
+        sf::RectangleShape stud5(sf::Vector2f(100.0f, 100.0f));
+    	stud5.setFillColor(sf::Color::Green);
+    	stud5.setPosition(1000.0f, 600.0f);
+        sf::RectangleShape stud6(sf::Vector2f(100.0f, 100.0f));
+    	stud6.setFillColor(sf::Color::Green);
+    	stud6.setPosition(1000.0f, 200.0f);
+        
     }
 
     void run()
