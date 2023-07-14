423f7e3b52bdceb9520069a8a4bbd8a7be0cd9af
Clinton <clintonarmando25@gmail.com>
2023-07-13 21:47:06 -0500

    movimiento aleatorio

--- game/main.cpp
+++ game/main.cpp
@@ -1,4 +1,8 @@
 #include <SFML/Graphics.hpp>
+#include<stdlib.h>
+#include<iostream>
+
+using namespace std;
 
 class Game
 {
@@ -8,6 +12,8 @@ public:
         square.setFillColor(sf::Color::Red);
         square.setPosition(0.0f, 400.0f);
         squareSpeed = 200.0f;
+        squareSpeedx = squareSpeed;
+        squareSpeedx = 0.0f;
         sf::RectangleShape stud1(sf::Vector2f(100.0f, 100.0f));
     	stud1.setFillColor(sf::Color::Green);
     	stud1.setPosition(200.0f, 600.0f);
@@ -54,13 +60,55 @@ private:
 
     void update(sf::Time elapsedTime)
     {
-        float movement = squareSpeed * elapsedTime.asSeconds();
+        float movement = squareSpeedx * elapsedTime.asSeconds();
         square.move(movement, 0.0f);
-
-        if (square.getPosition().x + square.getSize().x > window.getSize().x || square.getPosition().x < 0)
-        {
-            squareSpeed = -squareSpeed;
+		int numrand=0; 	
+        if (square.getPosition().x + square.getSize().x > window.getSize().x || square.getPosition().x < 0){
+        	numrand = 1+rand()%(3-1);
+            switch(numrand){
+            	case 1:
+            		squareSpeedx = -squareSpeedx;
+            		cout<<numrand;
+            		break;
+            	case 2:
+            		squareSpeedx = -squareSpeedx;
+            		cout<<numrand;
+            		break;
+            	case 3:
+            		squareSpeedx = -squareSpeedx;
+            		cout<<numrand;
+            		break;
+            	
+			}
         }
+        else if (square.getPosition().y + square.getSize().y > window.getSize().y || square.getPosition().y < 0){
+        	numrand = 1+rand()%(3-1);
+            switch(numrand){
+            	case 1:
+            		squareSpeedx = -squareSpeedx;
+            		break;
+            	case 2:
+            		squareSpeedx = -squareSpeedx;
+            		break;
+            	case 3:
+            		squareSpeedx = -squareSpeedx;
+            		break;
+            	
+			}
+		}
+        else if ((square.getPosition().x < 0 && square.getPosition().y < 0) || (square.getPosition().x < 0 && square.getPosition().y + square.getSize().y > window.getSize().y) || (square.getPosition().x + square.getSize().x > window.getSize().x && square.getPosition().y < 0) || (square.getPosition().x + square.getSize().x > window.getSize().x && square.getPosition().y + square.getSize().y > window.getSize().y)){
+        	numrand = 1+rand()%(3-1);
+            switch(numrand){
+            	case 1:
+            		squareSpeedx = -squareSpeedx;
+            		break;
+            	case 2:
+            		squareSpeedx = -squareSpeedx;
+            		break;
+            	
+			}
+		}
+		
     }
 
     void render()
@@ -73,6 +121,8 @@ private:
     sf::RenderWindow window;
     sf::RectangleShape square;
     float squareSpeed;
+    float squareSpeedx;
+    float squareSpeedy;
 };
 
 int main()
